e3fd9eba4cd9f502b5b895e5a82b11dc149c9cb4
Lotus Lofgren <LotusLofgren@gmail.com>
2020-02-26 19:02:51 -0800

    Update and rename 03_process_GO_output.R to 04_process_GO_output.R

--- scripts/CAFE/04_process_GO_output.R
+++ scripts/CAFE/04_process_GO_output.R
@@ -1,26 +1,26 @@
-setwd("~/bigdata/Suillus_comp_genomics/All_GOterms")
+setwd("~/bigdata/Suillus_comp_genomics/All_GOterms/fams_of_interest")
 options(stringsAsFactors = FALSE)
 
 #read in files
-expansions_files<- list.files(path ="~/bigdata/Suillus_comp_genomics/All_GOterms/", pattern = "GO_terms_output.test.expansions*", full.names=T)
-contractions_files<- list.files(path ="~/bigdata/Suillus_comp_genomics/All_GOterms/", pattern = "GO_terms_output.test.contractions*", full.names=T)
+expansions_files<- list.files(path ="~/bigdata/Suillus_comp_genomics/All_GOterms/fams_of_interest", pattern = "*expansions.clean.GO_terms_output.txt", full.names=T)
+contractions_files<- list.files(path ="~/bigdata/Suillus_comp_genomics/All_GOterms/fams_of_interest", pattern = "*contractions.clean.GO_terms_output.txt", full.names=T)
 
 #exclude empty files
 expansions_files <- expansions_files[which(file.info(expansions_files)$size>0)]
 contractions_files <- contractions_files[which(file.info(contractions_files)$size>0)]
 
-expansions<-lapply(expansions_files, fread, sep = "\t", header = FALSE, colClasses = 'character', stringsAsFactors=FALSE)
-contractions<-lapply(contractions_files, fread, sep = "\t", header = FALSE, colClasses = 'character', stringsAsFactors=FALSE)
+expansions<-lapply(expansions_files, fread, sep = " ", header = FALSE, colClasses = 'character', stringsAsFactors=FALSE)
+contractions<-lapply(contractions_files, fread, sep = " ", header = FALSE, colClasses = 'character', stringsAsFactors=FALSE)
+
 
 #keep names consistant with gene family #s
-expansions_names<- gsub(".*test.expansions_", "", expansions_files)
-expansions_names<- gsub(".txt", "", expansions_names)
+expansions_names<- gsub(".*fams_of_interest/", "", expansions_files)
+expansions_names<- gsub("_expansions.clean.GO_terms_output.txt", "", expansions_names)
 names(expansions)<- expansions_names
 
-contractions_names<- gsub(".*test.contractions_", "", contractions_files)
-contractions_names<- gsub(".txt", "", contractions_names)
+contractions_names<- gsub(".*fams_of_interest/", "", contractions_files)
+contractions_names<- gsub("_contractions.clean.GO_terms_output.txt", "", contractions_names)
 names(contractions)<- contractions_names
-
 
 
 #expansions<- rbindlist(sapply(expansions_names, fread, simplify = FALSE),
@@ -34,6 +34,7 @@ names(contractions)<- contractions_names
 expansions_table<- lapply(expansions, table)
 contractions_table<- lapply(contractions, table)
 
+
 #these are the gene families of interest (expanded in more than four species)
 expansions_table$`289`
 expansions_table$`279`
@@ -44,10 +45,7 @@ expansions_table$`311`
 expansions_table$`168`
 expansions_table$`320`
 expansions_table$`198`
-expansions_table$`233`
 expansions_table$`310`
-
-
 
 contractions_table$`61`
 contractions_table$`108`
@@ -66,7 +64,6 @@ contractions_table$`66`
 contractions_table$`126`
 contractions_table$`198`
 contractions_table$`247`
-
 contractions_table$`60`
 contractions_table$`127`
 contractions_table$`147`
